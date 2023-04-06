d484be9f39ffabb1a290c81e070b92c3572f88e0
vinicius-alexandre-lima <viniale0913@gmail.com>
2023-04-06 16:20:46 -0300

    Criei novas rotas

---
+++ src/pages/GerenciamentoPoluicao.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="q-py-md q-px-sm">
+    <h1>Gerenciamento Poluição</h1>
+    {{ a + b }}
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "GerenciamentoPoluicao",
+  data() {
+    return {
+      a: 1,
+      b:"Tatuapé"
+    };
+  },
+  async created() {
+    console.log("Iniciou aplicação")
+  },
+  methods: {
+    teste(){
+      console.log("Teste")
+    }
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
